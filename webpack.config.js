8b34d4f02be7def5666f5896f7a99e8c0e6973b7
Andrei Horodinca <andrei.horodinca@gmail.com>
2018-06-10 18:14:06 +0200

    update webpack.config

--- webpack.config.js
+++ webpack.config.js
@@ -9,29 +9,44 @@ module.exports = {
   entry: './src/client/index.js',
   output: {
     path: path.join(__dirname, outputDirectory),
-    filename: 'bundle.js'
+    filename: 'bundle.js',
+    publicPath: '/'
   },
   module: {
     rules: [
       {
-        test: /\.js$/,
+        test: /\.jsx?$/,
         exclude: /node_modules/,
         use: {
-          loader: 'babel-loader'
+          loader: 'babel-loader',
+          options: {
+            presets: [ 'es2015', 'react', 'stage-3' ]
+          }
         }
       },
       {
         test: /\.css$/,
         use: ['style-loader', 'css-loader']
+      },
+      {
+        test: /\.svg/,
+        use: {
+          loader: 'svg-url-loader',
+          options: {}
       }
+    }
     ]
   },
   devServer: {
     port: config.get('ports.client'),
     open: true,
+    historyApiFallback: true,
     proxy: {
       '/api': `http://localhost:${config.get('ports.server')}`
     }
+  },
+  node: {
+    fs: 'empty'
   },
   plugins: [
     new CleanWebpackPlugin([outputDirectory]),
