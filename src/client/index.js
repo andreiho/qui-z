b4475aa2d99dd79145b9be36c785ed65e7be73b4
Andrei Horodinca <andrei.horodinca@gmail.com>
2018-12-18 10:31:54 +0100

    test

--- src/client/index.js
+++ src/client/index.js
@@ -1,18 +1,17 @@
-import React from 'react';
-import { render } from 'react-dom';
-import { Provider } from 'react-redux';
-import axios from 'axios';
+import React from "react";
+import { render } from "react-dom";
+import { Provider } from "react-redux";
+import axios from "axios";
 
-import store from './helpers/store';
-import App from './components/App';
-import css from './app.css';
+import store from "./helpers/store";
+import App from "./components/App";
 
-axios.defaults.headers.common['Accept'] = 'application/json';
-axios.defaults.headers.common['Content-Type'] = 'application/json';
+axios.defaults.headers.common["Accept"] = "application/json";
+axios.defaults.headers.common["Content-Type"] = "application/json";
 
 render(
   <Provider store={store}>
     <App />
   </Provider>,
-  document.getElementById('app')
+  document.getElementById("app")
 );
